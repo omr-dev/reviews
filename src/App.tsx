353cb1a60a59f0ded36dadc0fb64f15fea8a3f2b
omer <oemer.peynirci@ontavio.de>
2022-11-18 12:09:15 +0100

    logic completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import './App.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faQuoteRight, faChevronRight,faChevronLeft} from '@fortawesome/free-solid-svg-icons';
+import {faQuoteRight, faChevronRight, faChevronLeft} from '@fortawesome/free-solid-svg-icons';
+import data from './data';
+import {useState} from 'react';
 
 function App() {
+    const [person, setPerson] = useState({...data[0], index: 0});
+    const loadPrePerson = () => {
+        let newIndex ;
+        if (person.index === 0) {
+            newIndex = data.length - 1;
+        } else {
+            newIndex = person.index - 1;
+        }
+        setPerson({...data[newIndex], index: newIndex});
+    };
+    const loadNextPerson = () => {
+        let newIndex ;
+        if (person.index === data.length - 1) {
+            newIndex = 0;
+        } else {
+            newIndex = person.index + 1;
+        }
+        setPerson({...data[newIndex], index: newIndex});
+    };
+    const loadRandomPerson=()=>{
+        const lastIndex=person.index;
+        let newIndex=lastIndex;
+        while(newIndex===lastIndex) {
+            newIndex = Math.floor(Math.random() * (data.length));
+        }
+        setPerson({...data[newIndex], index: newIndex});
+    }
 
     return (
         <div className="App">
@@ -12,17 +41,19 @@ function App() {
                     <div className="underline"></div>
                 </div>
                 <div className="review-box">
-                    <div className="person-image"><img src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg" alt="Susan Smith"/><div className="blue-frame"></div><div className="icon-background"><FontAwesomeIcon icon={faQuoteRight}/></div></div>
-                    <div className="person-name"><h4>peter jones</h4></div>
-                    <div className="person-title"><p>intern</p></div>
-                    <div className="person-comment"><p>Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing
-                        swag.</p></div>
+                    <div className="person-image"><img src={person.image} alt={person.name}/>
+                        <div className="blue-frame"></div>
+                        <div className="icon-background"><FontAwesomeIcon icon={faQuoteRight}/></div>
+                    </div>
+                    <div className="person-name"><h4>{person.name}</h4></div>
+                    <div className="person-title"><p>{person.job}</p></div>
+                    <div className="person-comment"><p>{person.text}</p></div>
                     <div className="nav-arrows">
-                        <FontAwesomeIcon icon={faChevronLeft} />
-                        <FontAwesomeIcon icon={faChevronRight}/>
+                        <FontAwesomeIcon icon={faChevronLeft} onClick={loadPrePerson} className="arrow left-arrow"/>
+                        <FontAwesomeIcon icon={faChevronRight} onClick={loadNextPerson} className="arrow right-arrow"/>
                     </div>
                     <div className="surprise-btn">
-                        <button>Surprise Me</button>
+                        <button onClick={loadRandomPerson}>Surprise Me</button>
                     </div>
                 </div>
 
